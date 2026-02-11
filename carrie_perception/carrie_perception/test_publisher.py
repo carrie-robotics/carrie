1cb8b8344f7370e027d1a38f4e4789db6fd8e034
laibazahid26 <laibazahid26@gmail.com>
2026-02-11 19:42:14 +0100

    continuously publish example image on a camera topic

--- carrie_perception/carrie_perception/test_publisher.py
+++ carrie_perception/carrie_perception/test_publisher.py
@@ -11,9 +11,9 @@ import numpy as np
 import os
 
 
-class ImageOncePublisher(Node):
+class ImagePublisher(Node):
     def __init__(self):
-        super().__init__('test_publisher')
+        super().__init__('image_publisher')
 
         # Parameters
         self.declare_parameters(
@@ -21,38 +21,41 @@ class ImageOncePublisher(Node):
             parameters=[
                 ('image_path', 'carrie_perception/example_images/solvay_conference_1927.jpg'),
                 ('topic', '/camera/image_raw'),
-            ]
-        )
+            ])
 
-        image_path = self.get_parameter('image_path').value
-        topic = self.get_parameter('topic').value
+        self.image_path = self.get_parameter('image_path').value
+        self.topic = self.get_parameter('topic').value
 
-        self.publisher = self.create_publisher(Image, topic, 10)
+        self.publisher = self.create_publisher(Image, self.topic, 10)
         self.bridge = CvBridge()
 
         # Load image
-        if not os.path.exists(image_path):
-            self.get_logger().error(f'Image not found: {image_path}')
+        if not os.path.exists(self.image_path):
+            self.get_logger().error(f'Image not found: {self.image_path}')
             rclpy.shutdown()
             return
 
-        pil_image = PILImage.open(image_path).convert('RGB')
+        pil_image = PILImage.open(self.image_path).convert('RGB')
         cv_image = np.array(pil_image)
 
-        msg = self.bridge.cv2_to_imgmsg(cv_image, encoding='rgb8')
-        msg.header.frame_id = 'camera'
+        self.msg = self.bridge.cv2_to_imgmsg(cv_image, encoding='rgb8')
+        self.msg.header.frame_id = 'camera'
 
-        # Publish once
-        self.publisher.publish(msg)
-        self.get_logger().info(
-            f'Published image once on {topic}: {image_path}'
-        )
+        self.timer = self.create_timer(5, self.timer_callback)
+        
+        self.get_logger().info(f'Publishing image continuously on {self.topic} ')
+
+    def timer_callback(self):
+        self.msg.header.stamp = self.get_clock().now().to_msg()
+        self.publisher.publish(self.msg)
 
 
 def main():
     rclpy.init()
-    node = ImageOncePublisher()
+    node = ImagePublisher()
     rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
 
 
 if __name__ == '__main__':
